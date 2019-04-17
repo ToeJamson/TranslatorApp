cbe0cba810aead95e4d9a04a61db3292e8dddd74
Joe Hanson <joe@pubnub.com>
2019-04-17 10:59:56 -0700

    updated

---
+++ precache-manifest.318e62dd01b263d47920a178a65d6527.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d532c439a381fd504b75",
+    "url": "/TranslatorApp/static/js/runtime~main.d532c439.js"
+  },
+  {
+    "revision": "b0fe1910fa532006ef47",
+    "url": "/TranslatorApp/static/js/main.b0fe1910.chunk.js"
+  },
+  {
+    "revision": "83eae9161c830d494ceb",
+    "url": "/TranslatorApp/static/js/1.83eae916.chunk.js"
+  },
+  {
+    "revision": "b0fe1910fa532006ef47",
+    "url": "/TranslatorApp/static/css/main.d26d40e1.chunk.css"
+  },
+  {
+    "revision": "fb4e56265576bf31d87217c8e4599ce4",
+    "url": "/TranslatorApp/index.html"
+  }
+];
